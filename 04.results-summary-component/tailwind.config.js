25b2b8ec5cbbb9cf3c02e62763c729f2c6aa5353
mhmdNoman <mhmdnoman01@gmail.com>
2023-12-20 18:52:27 +0600

    added new project

---
+++ 04.results-summary-component/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0A2640',
+        secondary: '#69E6A6',
+      }
+    },
+  },
+  plugins: [],
+}
