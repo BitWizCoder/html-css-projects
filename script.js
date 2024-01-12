0c063581d3f67910853c48747d3c247447ba3f95
mhmdnoman <mhmdnoman01@gmail.com>
2024-01-12 08:43:44 +0600

    updaetd grid layout

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function displayPorjects(projects) {
   projects.map((project, i) => {
     const id = i + 1;
     const projectItem = document.createElement("div");
-    projectItem.classList.add("card-width");
+    projectItem.classList.add("card");
 
     projectItem.innerHTML = `
                     <div>
                         <img class="img-width" src=${project.img} alt="">
                     </div>
                     <div class="project-info">
-                        <h3>${project.title}</h3>
+                        <h2 class="title">${project.title}</h2>
                         <p>${project.description}</p>
                         <a href=${project.live} target="_blank" class="btn btn-preview">Preview</a>
                         <a href=${project.github} target="_blank" class="btn btn-preview">Code</a>
